eb35d35a9d6fa479866c64e4011ce758ec099b2c
devsyedhaider <devalihaider86@gmail.com>
2026-03-16 16:00:51 +0500

    done star rating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,34 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.jsx";
 import StarRating from "./StarRating";
 
+export default function Test() {
+  const [MovieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="blue"
+        size={36}
+        onSetRating={setMovieRating}
+      />
+      <p>This movies was {MovieRating} rated Stars</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      color="red"
+      size={35}
+      defaultRating={2}
+    />
+    <Test />
   </StrictMode>,
 );
